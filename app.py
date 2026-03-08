f7fb9d4c0ecc3f890aa07b6c97ae37079f844deb
Mariel Estosane <estosanemariel@gmail.com>
2026-03-08 19:59:02 +0800

    Implement student profile builder using Streamlit
    Added a Streamlit UI for student profile input.

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
+import streamlit as st
 
+st.title("🎓 Student Profile Builder")
+
+st.write("Welcome! This is my Streamlit UI Assignment.")
+
+name = st.text_input("Enter your name")
+
+age = st.number_input("Enter your age", 1, 100)
+
+course = st.selectbox(
+    "Select your course",
+    ["BSIT", "BSCS", "BSIS"]
+)
+
+if st.button("Submit"):
+    st.success("Form submitted!")
+    st.write("Name:", name)
+    st.write("Age:", age)
+    st.write("Course:", course)
